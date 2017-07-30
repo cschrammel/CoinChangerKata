93f701384b0b1ffa588c1e827cc73c50b88de2a3
cschrammel <carl.schrammel@gmail.com>
2017-07-30 11:25:18 -0500

    Back to square 1

--- CoinChangerKata.Tests/CoinChangerTests.cs
+++ CoinChangerKata.Tests/CoinChangerTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 
 namespace CoinChangerKata.Tests
 {
@@ -13,68 +12,6 @@ namespace CoinChangerKata.Tests
             var result = changer.GetChange(0, new[] {1});
 
             Assert.Equal(new[] { 0 }, result);
-        }
-
-        [Fact]
-        public void NintyNineCentsInPennies()
-        {
-            var changer = new CoinChanger();
-
-            var result = changer.GetChange(99, new[] { 1 });
-
-            Assert.Equal(new[] { 99 }, result);
-        }
-
-        [Fact]
-        public void FiveCentsInPenniesAndNickles()
-        {
-            var changer = new CoinChanger();
-
-            var result = changer.GetChange(5, new[] { 1, 5 });
-
-            Assert.Equal(new[] { 0, 1 }, result);
-        }
-
-
-        [Fact]
-        public void SixCentsInPenniesAndNickles()
-        {
-            var changer = new CoinChanger();
-
-            var result = changer.GetChange(6, new[] { 1, 5 });
-
-            Assert.Equal(new[] { 1, 1 }, result);
-        }
-
-        [Fact]
-        public void SixCentsInPenniesAndNicklesAndDimes()
-        {
-            var changer = new CoinChanger();
-
-            var result = changer.GetChange(15, new[] { 10, 5, 1 });
-
-            Assert.Equal(new[] { 1, 1, 0 }, result);
-        }
-
-
-        [Fact]
-        public void BigTest()
-        {
-            var changer = new CoinChanger();
-
-            var result = changer.GetChange(99, new[] { 50, 25, 10, 5, 1 });
-
-            Assert.Equal(new[] { 1, 1, 2, 0, 4 }, result);
-        }
-
-        [Fact]
-        public void BigTest2()
-        {
-            var changer = new CoinChanger();
-
-            var result = changer.GetChange(99, new[] { 25, 10, 5, 1 });
-
-            Assert.Equal(new[] { 3, 2, 0, 4 }, result);
         }
     }
 }
--- CoinChangerKata/CoinChanger.cs
+++ CoinChangerKata/CoinChanger.cs
@@ -1,26 +1,11 @@
-﻿using System.Linq;
-
-namespace CoinChangerKata
+﻿namespace CoinChangerKata
 {
     public class CoinChanger
     {
-        public CoinChanger()
-        {
-        }
 
         public int[] GetChange(int value, int[] coins)
         {
-            var change = new int[coins.Length];
-            var coinsList = coins.ToList();
-            foreach (var c in coinsList.OrderByDescending(l=>l))
-            {
-                while (value >= c)
-                {
-                    change[coinsList.IndexOf(c)] += 1;
-                    value -= c;
-                }
-            }
-            return change;
+            return new int[coins.Length];
         }
     }
 }
